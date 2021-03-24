da657b77c35cc7fbdf3cd1def396501f25be9cdc
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2021-03-24 20:19:11 -0300

    Entrega Desafio 1 CLI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Fragment } from 'react'
 
-function App() {
+export default function App() {
+
+  const name = "Agustin";
+  const edad = 29;
+  const style = {
+    color: 'orange',
+    fontSize: 16
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <h1 className="titulo-h1">Hola Mundo!</h1>
+      <p style={style}>Mi nombre es {name} y tengo {edad} años.</p>
+    </Fragment>
+  )
 }
 
-export default App;
