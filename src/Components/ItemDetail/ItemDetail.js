b4b2ed5419b9286bbd17ab2463d14f674cd2b7ec
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2021-04-15 17:08:59 -0300

    Desafio clase 7

--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -9,44 +9,24 @@ export default function ItemDetail({ products }) {
 
     return (
         <Fragment>
-            <div className="wrapper">
-                <div className="product-detail-container">
-                    <div className="product-detail-container-img">
-                        <img src={productos[0].image} alt=""></img>
+            {productos.map((producto) => {
+                return (
+                    < div className="wrapper">
+                        <div className="product-detail-container">
+                            <div className="product-detail-container-img">
+                                <img src={producto.image} alt=""></img>
+                            </div>
+                            <div className="product-detail-container-info">
+                                <h2 className="detail-product-title">{producto.title}</h2>
+                                <h4 className="detail-product-price">{producto.price}</h4>
+                                <p className="detail-product-desc">{producto.description}</p>
+                                <button className="boton-product">Agregar al carrito</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="product-detail-container-info">
-                        <h2 className="detail-product-title">{productos[0].title}</h2>
-                        <h4 className="detail-product-price">{productos[0].price}</h4>
-                        <p className="detail-product-desc">{productos[0].description}</p>
-                        <button className="boton-product">Agregar al carrito</button>
-                    </div>
-                </div>
-            </div>
-        </Fragment>
+                )
+            })
+            }
+        </Fragment >
     )
 }
-
-//     return (
-//         <Fragment>
-//             <div className="wrapper">
-//                 {
-//                     products.map((product) => {
-//                         return (
-//                             <div className="product-detail-container">
-//                                 <div className="product-detail-container-img">
-//                                     <img src={product.image} alt=""></img>
-//                                 </div>
-//                                 <div className="product-detail-container-info">
-//                                     <h2 className="detail-product-title">{product.title}</h2>
-//                                     <h4 className="detail-product-price">{product.price}</h4>
-//                                     <p className="detail-product-desc">{product.description}</p>
-//                                     <button className="boton-product">Agregar al carrito</button>
-//                                 </div>
-//                             </div>
-//                         )
-//                     })
-//                 }
-//             </div>
-//         </Fragment>
-//     )
-// }
